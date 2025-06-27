5d2638440c50c3c6e64c9df2cda9229fe39cd664
Thierry Henry <167599222+ThierryHenryTavares@users.noreply.github.com>
2025-06-27 14:50:04 +0000

    Versao final 4

--- routes/contatoRoutes.js
+++ routes/contatoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contatoController = require('../controllers/contatoController');
+const contatoController = require('../controller/contatoController');
 
 router.get('/', contatoController.getAllContatos);
 router.get('/:id', contatoController.getContatoById);
