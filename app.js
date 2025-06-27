4347a1e43ff0c6e83ee37462f3eb5ac9aed25216
Thierry Henry <167599222+ThierryHenryTavares@users.noreply.github.com>
2025-06-27 14:57:57 +0000

    Versao final 7

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
+
 const dotenv = require('dotenv');
-const conectarBanco = 이러닝'./config/db');
+
+const conectarBanco = require('./config/db');
+
 const contatoRoutes = require('./routes/contatoRoutes');
+
 const userRoutes = require('./routes/userRoutes');
+
 const authMiddleware = require('./middleware/authMiddleware');
+
+
 
 dotenv.config();
 
+
+
 const app = express();
+
 app.use(express.json());
 
-// Suas rotas da API
+
+
 app.use('/api/usuarios', userRoutes); // públicas
+
 app.use('/api/contatos', authMiddleware, contatoRoutes); // protegidas
 
-// Nova rota para a raiz do seu serviço
-app.get('/', (req, res) => {
-  res.send('API de Contatos está funcionando! Acesse /api/usuarios ou /api/contatos.');
-});
+
 
 conectarBanco();
 
+
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
